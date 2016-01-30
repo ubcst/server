77408617b17ae13592ae0b25d03b39998ee789e1
treyu <trevor.tyu@gmail.com>
2016-01-30 16:19:47 -0500

    Added gitignore and alias to copy and source other aliases

---
+++ svn/init/aliases/cpalias.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Alias that can be used to copy an alias to the /etc/profile.d/ directory
+# Also sources the file so that it can be run right away
+printUsage() {
+    # Print the usage message
+    echo "usage: cpalias aliasfile"
+    echo -e "   aliasfile:\tThe alias file to copy to /etc/profile.d/"
+}
+
+cpalias() {
+    FILE=$1
+
+    # Check the number of arguments passed in
+    if [ $# -lt 1 ]; then
+        echo "Error: Incorrect number of arguments"
+        printUsage
+        return
+    fi
+
+    # Check if the file exists
+    if [ ! -e "$FILE" ]; then
+        echo "Error: File does not exist"
+        printUsage
+        return
+    fi
+
+    sudo cp $FILE /etc/profile.d/
+    source /etc/profile.d/$FILE
+
+    echo "Alias updated"
+}
