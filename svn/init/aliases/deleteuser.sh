83b6de60b6cf9829b1da709fa53c6fce594c7397
treyu <trevor.tyu@gmail.com>
2016-02-01 04:06:09 -0500

    Added stuff to remove a user's /home/ directory

--- svn/init/aliases/deleteuser.sh
+++ svn/init/aliases/deleteuser.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # Alias that deletes a user from having SSH and SVN access
+# Also deletes their /home/ directory
 deleteuser() {
-    USAGE_STATEMENT="Deletes a user from having SSH and SVN access\n"
+    USAGE_STATEMENT="Deletes a user from having SSH and SVN access and deletes\n"
+    USAGE_STATEMENT+="their /home/<username>/ directory\n"
     USAGE_STATEMENT+="   usage: deleteuser username\n"
 
+    HOME_DIR="/home/"
     SVN_PASSWD_DIR="/home/svn/.htpasswd"
     USERNAME=""
 
@@ -21,6 +24,12 @@ deleteuser() {
     fi
 
     USERNAME=$1
+
+    # Check if user has SSH access and delete their folder
+    if [ -d "$HOME_DIR$USERNAME/" ]; then
+        echo -e "Deleting $HOME_DIR$USERNAME/"
+        sudo rm -rf $HOME_DIR$USERNAME
+    fi
 
     echo -e "Removing SVN access for \`$USERNAME'..."
     sudo htpasswd -D $SVN_PASSWD_DIR $USERNAME
